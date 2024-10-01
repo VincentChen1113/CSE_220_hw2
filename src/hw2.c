cc31900bb1c62453c5e18a1bf145ecc3c8b43c3d
VincentChen1113 <158762988+VincentChen1113@users.noreply.github.com>
2024-10-01 04:37:23 +0000

    address

--- src/hw2.c
+++ src/hw2.c
@@ -12,15 +12,19 @@ void print_packet(unsigned int packet[])
     unsigned int header1 = packet[1];
     unsigned int header2 = packet[2];
 
-    unsigned int packet_type = header0 >> 10;
+    /*unsigned int packet_type = header0 >> 10;
     unsigned int length = header0 & 0x3FF;
     unsigned int address = ((header2 >> 2) << 2) & 0xFFFFFFFF;
     unsigned int requester_ID = (header1 >> 16) & 0xFFFF;
     unsigned int tag = (header1 >> 8) & 0xFF;
     unsigned int last_BE = (header1 >> 4) & 0xF;
-    unsigned int first_BE = header1 & 0xF;
+    unsigned int first_BE = header1 & 0xF;*/
 
-    if(packet_type == 0x1FFFFF){
+    printf("%08X\n",header0);
+    printf("%08X\n",header1);
+    printf("%08X\n",header2);
+
+    /*if(packet_type == 0x1FFFFF){
         printf("Packet Type: Write");
     }
     else if(packet_type == 0){
@@ -43,7 +47,7 @@ void print_packet(unsigned int packet[])
             }
             printf("%d, ", (int)packet[data_start_point++]);
         }
-    }
+    }*/
 }
 
 void store_values(unsigned int packets[], char *memory)
