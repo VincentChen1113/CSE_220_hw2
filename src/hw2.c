fddc3c9be6ee8054b065b3b7ed5fa5b722324a93
VincentChen1113 <158762988+VincentChen1113@users.noreply.github.com>
2024-10-01 19:49:39 +0000

    part 2

--- src/hw2.c
+++ src/hw2.c
@@ -52,10 +52,78 @@ void print_packet(unsigned int packet[])
     }
 }
 
-void store_values(unsigned int packets[], char *memory)
-{
-    (void)packets;
-    (void)memory;
+void store_values(unsigned int packet[], char *memory){
+    unsigned int header0 = packet[0];
+    unsigned int header1 = packet[1];
+    unsigned int header2 = packet[2];
+
+    unsigned int packet_type = (header0 >> 10) & 0x3FFFFF;
+    unsigned int length = header0 & 0x3FF;
+    unsigned int address = header2 & 0x3FFFFFFF;
+    unsigned int last_BE = (header1 >> 4) & 0xF;
+    unsigned int first_BE = header1 & 0xF;
+
+    
+    unsigned int memory_index = address;
+    int data_index = 3;
+    unsigned int data = packet[data_index];
+
+    if(packet_type == 0x1FFFFFFF){
+        if(length > 0 && address < 0x100000){
+            //writing memory for 1st BE
+            if(first_BE & 1)
+                memory[memory_index++] = (data & 0xFF);
+            else 
+                memory_index++;
+
+            if(first_BE & 2)
+                memory[memory_index++] = ((data >> 2) & 0xFF);
+            else
+                memory_index++;
+
+            if(first_BE & 4)
+                memory[memory_index++] = ((data >> 4)& 0xFF);
+            else
+                memory_index++;
+
+            if(first_BE & 8)
+                memory[memory_index++] = ((data >> 6) & 0xFF);
+            else
+                memory_index++;
+
+            if(length > 2){
+                for(unsigned int i = 1; i < length - 1; i++){
+                    data_index = data_index + i;
+                    memory[memory_index++] = (data & 0xFF);
+                    memory[memory_index++] = ((data >> 2) & 0xFF);
+                    memory[memory_index++] = ((data >> 4)& 0xFF);
+                    memory[memory_index++] = ((data >> 6) & 0xFF);
+                }
+            }
+
+            //writing memory for last BE
+            unsigned int last_data_index = length + 2;
+            unsigned int last_data = packet[last_data_index];
+            if(last_BE & 1)
+                memory[memory_index++] = (last_data & 0xFF);
+            else
+                memory_index++;
+            if(last_BE & 2)
+                memory[memory_index++] = ((last_data >> 2) & 0xFF);
+            else
+                memory_index++;
+            if(last_BE & 4)
+                memory[memory_index++] = ((last_data >> 4) & 0xFF);
+            else
+                memory_index++;
+            if(last_BE & 8)
+                memory[memory_index++] = ((last_data >> 6) & 0xFF);
+            else
+                memory_index++;
+        }
+    }
+    
+
 }
 
 unsigned int* create_completion(unsigned int packets[], const char *memory)
