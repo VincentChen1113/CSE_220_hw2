2189d3b47daca065c72b91629913669a4e3477bb
VincentChen1113 <158762988+VincentChen1113@users.noreply.github.com>
2024-10-05 01:51:24 +0000

    part3

--- src/hw2.c
+++ src/hw2.c
@@ -140,12 +140,13 @@ unsigned int* create_completion(unsigned int packets[], const char *memory){
     unsigned int *completed_pockets = malloc(262144 * sizeof(unsigned int)); //1 MB pocket to store and return
     if(completed_pockets == NULL)
         return NULL;
+
     unsigned int current_packets_index = 0;
     unsigned int complete_read_index = 0;
 
-    unsigned int header_type = 37;
+    unsigned int header_type = 0x25;
     header_type <<= 25;
-    unsigned int completer_id = 220;
+    unsigned int completer_id = 0xDC;
     completer_id <<= 16;
     unsigned int byte_count = 0;
 
@@ -168,73 +169,77 @@ unsigned int* create_completion(unsigned int packets[], const char *memory){
 
 
         if(packet_type == 0){
-            if((address + length) > 0x4000){
-                completed_pockets[complete_read_index++] = (header_type | length); //completion header0 wrote
+            if((address + length) >= 0x4000){
+                unsigned int exceed = (address + length) - 0x4000;
+                completed_pockets[complete_read_index++] = (header_type | (length - exceed)); //completion header0 wrote
                 completed_pockets[complete_read_index++] = (completer_id | (byte_count & 0xFFF)); //completion header1 wrote
-                completed_pockets[complete_read_index++] = ((requester_ID << 16) | (tag << 8) | lower_address); ////completion header2 wrote
+                completed_pockets[complete_read_index++] = ((requester_ID << 16) | (tag << 8) | lower_address ); ////completion header2 wrote
+                
                 unsigned int data = 0;
+
                 if(first_BE & 1) // 0b0001
                     data |= (memory[memory_index++] & 0xFF);
                 else 
                     memory_index++;
 
-                if(first_BE & 2)
-                    data |= (memory[memory_index++] & 0xFF00);
+                if(first_BE & 2) // 0b0010
+                    data |= ((memory[memory_index++] << 8) & 0xFF00);
                 else
                     memory_index++;
 
                 if(first_BE & 4)
-                    data |= (memory[memory_index++] & 0xFF0000);
+                    data |= ((memory[memory_index++] << 16) & 0xFF0000);
                 else
                     memory_index++;
 
                 if(first_BE & 8)//0b1000
-                    data |= (memory[memory_index++] & 0xFF000000);
+                    data |= ((memory[memory_index++] << 24) & 0xFF000000);
                 else
                     memory_index++;
                 
                 completed_pockets[complete_read_index++] = data;
 
-                for(unsigned int i = address + 1; i <= 0x4000; i++){
+                for(unsigned int i = 1; i < (length - exceed); i++){
                     data = 0;
                     data |= (memory[memory_index++] & 0xFF);
-                    data |= (memory[memory_index++] & 0xFF00);
-                    data |= (memory[memory_index++] & 0xFF0000);
-                    data |= (memory[memory_index++] & 0xFF000000);
+                    data |= ((memory[memory_index++] << 8) & 0xFF00);
+                    data |= ((memory[memory_index++] << 16) & 0xFF0000);
+                    data |= ((memory[memory_index++] << 24) & 0xFF000000);
                     completed_pockets[complete_read_index++] = data;// put in data
                 } //address exceed 0x4000 split to next packet
 
-                length = length  - ((unsigned int)0x4000 - address); //update length
+                length -= exceed; //update length
                 byte_count = length * 4; //update byte count
                 lower_address = 0; //update low address
                 completed_pockets[complete_read_index++] = (header_type | length); //completion header0 wrote
                 completed_pockets[complete_read_index++] = (completer_id | (byte_count & 0xFFF)); //completion header1 wrote
                 completed_pockets[complete_read_index++] = ((requester_ID << 16) | (tag << 8) | lower_address); ////completion header2 wrote
                 
-                for(unsigned int i = 0; i < length; i++){
+                for(unsigned int i = 0; i < length - 1; i++){
                     data = 0;
                     data |= (memory[memory_index++] & 0xFF);
-                    data |= (memory[memory_index++] & 0xFF00);
-                    data |= (memory[memory_index++] & 0xFF0000);
-                    data |= (memory[memory_index++] & 0xFF000000);
+                    data |= ((memory[memory_index++] << 8) & 0xFF00);
+                    data |= ((memory[memory_index++] << 16) & 0xFF0000);
+                    data |= ((memory[memory_index++] << 24) & 0xFF000000);
                     completed_pockets[complete_read_index++] = data;// put in data
                 }
 
                 data = 0; //reset data
+                
                 if(last_BE & 1)
                     data |= (memory[memory_index++] & 0xFF);
                 else
                     memory_index++;
                 if(last_BE & 2)
-                    data |= (memory[memory_index++] & 0xFF00);
+                    data |= ((memory[memory_index++] << 8) & 0xFF00);
                 else
                     memory_index++;
                 if(last_BE & 4)
-                    data |= (memory[memory_index++] & 0xFF0000);
+                    data |= ((memory[memory_index++] << 16) & 0xFF0000);
                 else
                     memory_index++;
                 if(last_BE & 8)
-                     data |= (memory[memory_index++] & 0xFF000000);
+                     data |= ((memory[memory_index++] << 24) & 0xFF000000);
                 else
                     memory_index++;  
                 
@@ -253,55 +258,54 @@ unsigned int* create_completion(unsigned int packets[], const char *memory){
                     memory_index++;
 
                 if(first_BE & 2)
-                    data |= (memory[memory_index++] & 0xFF00);
+                    data |= ((memory[memory_index++] << 8) & 0xFF00);
                 else
                     memory_index++;
 
                 if(first_BE & 4)
-                    data |= (memory[memory_index++] & 0xFF0000);
+                    data |= ((memory[memory_index++] << 16) & 0xFF0000);
                 else
                     memory_index++;
 
                 if(first_BE & 8)//0b1000
-                    data |= (memory[memory_index++] & 0xFF000000);
+                    data |= ((memory[memory_index++] << 24) & 0xFF000000);
                 else
                     memory_index++;
                 
                 completed_pockets[complete_read_index++] = data;
 
-                for(unsigned int i = 1; i < length; i++){
+                for(unsigned int i = 1; i < length - 1; i++){
                     data = 0;
                     data |= (memory[memory_index++] & 0xFF);
-                    data |= (memory[memory_index++] & 0xFF00);
-                    data |= (memory[memory_index++] & 0xFF0000);
-                    data |= (memory[memory_index++] & 0xFF000000);
+                    data |= ((memory[memory_index++] << 8) & 0xFF00);
+                    data |= ((memory[memory_index++] << 16) & 0xFF0000);
+                    data |= ((memory[memory_index++] << 24) & 0xFF000000);
                     completed_pockets[complete_read_index++] = data;// put in data
                 }
 
                 data = 0; //reset data
+                
                 if(last_BE & 1)
                     data |= (memory[memory_index++] & 0xFF);
                 else
                     memory_index++;
                 if(last_BE & 2)
-                    data |= (memory[memory_index++] & 0xFF00);
+                    data |= ((memory[memory_index++] << 8) & 0xFF00);
                 else
                     memory_index++;
                 if(last_BE & 4)
-                    data |= (memory[memory_index++] & 0xFF0000);
+                    data |= ((memory[memory_index++] << 16) & 0xFF0000);
                 else
                     memory_index++;
                 if(last_BE & 8)
-                    data |= (memory[memory_index++] & 0xFF000000);
+                     data |= ((memory[memory_index++] << 24) & 0xFF000000);
                 else
-                    memory_index++;  
+                    memory_index++; 
                 
                 completed_pockets[complete_read_index++] = data;
-            }
-            
-
+            }    
         }
-        else{
+        else{ // request type is not read
             current_packets_index += length;
             continue;
         }
